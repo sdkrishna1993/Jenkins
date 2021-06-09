b4eee4f23a308e08e76533e317359870214ed9f7
Family <Family@DESKTOP-OKV027T>
2021-06-09 12:38:55 +0530

    few changes

---
+++ JenkinsDemo/src/main/java/JenkinsDemo/JenkinsDemo/IdentifyProblemsInCode.java
@@ -0,0 +1,30 @@
+package JenkinsDemo.JenkinsDemo;
+import java.util.*; 
+
+public class IdentifyProblemsInCode {
+	
+	int i=10;
+	
+		 
+	    public void howToDoInJava_method1() {
+	        System.out.println("how to do");
+	        return;
+	    }
+	 
+	    public void howToDoInJava_method2() {
+	        System.out.println("how to do");
+	        if (true) {
+	            return;
+	        }
+	        System.out.println("in java");
+	    }
+	 
+	    public void howToDoInJava_method3() {
+	        System.out.println("how to do");
+	        while (true) {
+	            return;
+	        }
+	    }
+	}
+
+
